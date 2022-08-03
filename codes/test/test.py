fbd69b79ab4f618ca63b6fbd9bd956ff117e27ec
wei_lin <wei1234c@gmail.com>
2022-08-03 10:13:05 +0800

    rename files.

--- codes/test/test.py
+++ codes/test/test.py
@@ -1,6 +1,6 @@
 from transcriber import SRT
 
-fn_srt = 'test.srt'
+fn_srt = 'input.srt'
 srt = SRT(fn_srt)
 
 print(srt.get_script(3))
@@ -10,4 +10,4 @@ print(srt.get_script(5))
 
 print(srt.merge (3, 4, 5))
 print(srt.scripts[:8])
-srt.dump('test1.txt')
+srt.dump('output.srt')
